2f29e3559db12475a97ce35ae9e50f2f81ddb5a8
huangbintao188 <119724472+huangbintao188@users.noreply.github.com>
2023-11-21 22:27:11 +0800

    Update personal_config.sh

--- docker_images/svin2/melodic/scripts/personal_config.sh
+++ docker_images/svin2/melodic/scripts/personal_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --global user.name "Shuqi Xiao"
-git config --global user.email "xiaosq2000@gmail.com"
+git config --global user.name "tao"
+git config --global user.email "1227861021@qq.com"
 echo "source /opt/ros/kinetic/setup.bash" >> /root/.bashrc
 echo "source /usr/app/svin2_ws/devel/setup.bash" >> /root/.bashrc
